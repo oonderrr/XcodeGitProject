7751af4a0ffcf2a685944122f0dbe0d08c55d916
oonderrr <onder.bugdaci@gmail.com>
2023-10-26 15:16:10 +0300

    Initial Commit

---
+++ XcodeGitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XcodeGitProject
+//
+//  Created by Önder B on 26.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
